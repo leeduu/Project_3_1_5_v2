845affdf47ce627bdd2e2f3a1d0b3e26c2afbeca
leeduu <lidiya_by@mail.ru>
2023-04-10 23:05:39 +0300

    10.04

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -56,86 +56,63 @@ async function authUser() {
         })
 }
 
+
 // Модальное окно Edit user
-document.querySelector("#editUserButton").onclick = function() {
-    editUser();
-}
-async function editUser(id) {                                                    // ЮЗЕР НЕ ОБНОВЛЯЕТСЯ
-    fetch("http://localhost:8080/api/users/" + id)
-        .then(function (response) {  // to do sth with server response
-            return response.json();
-        })
-        .then(user => {
-            let form = document.forms["editUserForm"];
-            form.editUserId.value = user.id;
-            form.editUserUsername.value = user.username;
-            form.editUserPassword.value = user.password;
-            form.editUserEmail.value = user.email;
-            fetch("http://localhost:8080/api/roles")
-                .then(res => res.json())
-                .then(roles => {
-                    roles.forEach(role => {
-                        $('#editUserRoles').append($('<option/>').attr("value", role.id).text(role.name.replace("ROLE_", "")));
-                    })
-                })
-            $('#editUserModal').modal('show');
-        })
-}
+// document.querySelector("#editUserButton").onclick = function() {
+//     editUser();
+// }
 
-
-
-
-    // form.addEventListener("submit", ev => {
-    //     ev.preventDefault();
-    //     let editUserRoles = [];
-    //     if (form.roles !== undefined) {
-    //         for (let i = 0; i < form.roles.options.length; i++) {
-    //             if (form.roles.options[i].selected) editUserRoles.push({
-    //                 id: form.roles.options[i].value,
-    //                 name: "ROLE_" + form.roles.options[i].text
-    //             })
-    //         }
-    //     }
-    //     fetch("http://localhost:8080/api/users/" + id, {
-    //         method: 'PATCH',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({
-    //             id: form.id.value,
-    //             username: form.username.value,
-    //             password: form.password.value,
-    //             email: form.email.value,
-    //             roles: editUserRoles,
-    //         })
-    //     }).then(() => {
-    //         $('#closeEditUserFormButton').click();
-    //         allUsers();
-    //     })
-    // })
-
-
-
-
-
-
-// EXAMPLE  ================================================================
-// console.log("change started")
+// async function editUser(id) {                                                    // ЮЗЕР НЕ ОБНОВЛЯЕТСЯ
+//     fetch("http://localhost:8080/api/users/" + id)
+//         .then(function (response) {  // to do sth with server response
+//             return response.json();
+//         })
+//         .then(user => {
+//             let form = document.forms["editUserForm"];
+//             form.addEventListener("submit", event => {
+//                 event.preventDefault();
+//                 form.editUserId.value = user.id;
+//                 form.editUserUsername.value = user.username;
+//                 form.editUserPassword.value = user.password;
+//                 form.editUserEmail.value = user.email;
+//                 // form.editUserRoles.value = ...;
 //
-// $(document).on('click', '#changeButton', function() {
-//     let id = $(this).attr('data-userID');
-//     console.log('Кнопка ИЗМЕНИТЬ пользователя №' + id + ' нажата!');
-//     let url = 'http://localhost:8080/api/users/' + id;
-//     let body = 'Loading user #' + id + 'data...'
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(data => changingData(data))
-//         .catch(error => console.log(error))
+//                 fetch("http://localhost:8080/api/roles")
+//                     .then(res => res.json())
+//                     .then(roles => {
+//                         roles.forEach(role => {
+//                             $('#editUserRoles').append($('<option>').attr("value", role.id).text(role.name.replace("ROLE_", "")));
+//                         })
+//                     })
+//                 $('#editUserModal').modal('show');
+//             })
 //
-//     document.getElementById('changing').innerHTML = body; });
 //
-// console.log("change ended")
-// =========================================================================
+//             fetch("http://localhost:8080/api/users/" + id, {
+//                 method: 'PATCH',
+//                 headers: {
+//                     'Content-Type': 'text/javascript'
+//                 },
+//                 body: JSON.stringify({
+//                     id: form.editUserId.value,
+//                     username: form.editUserUsername.value,
+//                     password: form.editUserPassword.value,
+//                     email: form.editUserEmail.value,
+//                     // roles: editUserRoles
+//                 })
+//             }).then(() => {
+//                 $('#submitEditUserFormButton').click();
+//                 allUsers();
+//             }).then(() => {       // очистка поля editUserRoles, чтобы не копились роли
+//                 $("#editUserRoles").empty();
+//             })
+//
+//         })
+// }
+
+
+
+
 
 // Модальное окно Delete user
 document.querySelector("#deleteUserButton").onclick = function() {
@@ -162,43 +139,7 @@ async function deleteUser(id) {
             $('#deleteUserModal').modal('show');
         })
 }
-// v1
-$(async function() {                                                    // ЮЗЕР НЕ УДАЛЯЕТСЯ
-    deleteUserSubmit();
-});
-function deleteUserSubmit(id){
-    const deleteForm = document.forms["deleteUserForm"];
-    deleteForm.addEventListener("submit", ev => {
-        ev.preventDefault();
-        fetch("http://localhost:8080/api/users/" + id, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(user => {
-                $('#submitDeleteUserFormButton').click();
-                user.remove();
-                allUsers();
-            })
-    })
-}
-// v2
-// $(async function() {                                                    // ЮЗЕР НЕ УДАЛЯЕТСЯ
-//     deleteUserSubmit();
-// });
-// function deleteUserSubmit(){
-//     document.getElementById('submitDeleteUserFormButton').addEventListener('submit', (e) => {
-//         e.preventDefault();
-//         const id = document.getElementById('deleteUserId').textContent
-//         fetch("http://localhost:8080/api/users/" + id, {
-//             method: "DELETE",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         })
-//     })
-// }
+
 
 
 
@@ -206,8 +147,49 @@ function deleteUserSubmit(id){
 
 
 // New User                                                         // юзер не добавился, в адресной строке
-// fetch('http://localhost:8080/api/users/new', {
-//     method: 'POST',
-//     body: new FormData( document.getElementById('newUserForm') )
-// });
+$(async function() {
+    await newUser();
+});
+async function newUser() {
+    await fetch("http://localhost:8080/api/roles")
+        .then(res => res.json())
+        .then(roles => {
+            roles.forEach(role => {
+                $('#newRoles').append($('<option>').attr("value", role.id).text(role.name.replace("ROLE_", "")));
+            })
+        })
+
+    const form = document.querySelector("#newUserForm");
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+
+        let newRoles = [];
+        if (form.roles !== undefined) {
+            for (let i = 0; i < form.roles.length; i++) {
+                if (form.roles[i].selected) newRoles.push({
+                    id: form.roles[i].value,
+                    name: "ROLE_" + form.roles[i].text
+                })
+            }
+        }
+
+        fetch("http://localhost:8080/api/users/new", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: form.newUsername.value,
+                email: form.newEmail.value,
+                password: form.newPassword.value,
+                roles: newRoles
+            })
+        }).then(res => res.json())
+          .then(() => {
+              form.reset();                         // не показывает таблицу + ID увеличивается на 3
+              allUsers();
+              $('#users-table-pane').click("show");
+          })
+    });
+}
 
